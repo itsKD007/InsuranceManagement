21938bfd809f38d73a31356327718564ca0304e4
itsKD007 <78605466+itsKD007@users.noreply.github.com>
2022-11-05 11:20:43 +0530

    changed text line height and text -> paragraph conversion logic

--- src/frontend/routes/Home.ts
+++ src/frontend/routes/Home.ts
@@ -2,13 +2,11 @@ import { el, RedomComponent } from 'redom';
 
 export default class Home implements RedomComponent {
 
-  text = `
-We are happy to have you here. Please use the sidebar to navigate our app. You may need to log in to use certain features. If ever you need help, please do not hesistate to use the chat box.\n
+  text = `We are happy to have you here. Please use the sidebar to navigate our app. You may need to log in to use certain features. If ever you need help, please do not hesistate to use the chat box.
 
-We hope you have a pleasant experience using our app!\n
+We hope you have a pleasant experience using our app!
 
-If you liked using or application, or want us to improve anything, please leave us some feedback. We value your thoughts more than anyone!
-  `;
+If you liked using or application, or want us to improve anything, please leave us some feedback. We value your thoughts more than anyone!`;
 
   textElem = el('div.text');
 
@@ -17,6 +15,6 @@ If you liked using or application, or want us to improve anything, please leave 
     el('h2', "Welcome to KD Insurance!"),
     el('hr'));
 
-  el = el('div.view.pure-u-1', this.header, this.text.split('\n').map(text => el('p', text)));
+  el = el('div.view.pure-u-1', this.header, this.text.split('\n\n').map(text => el('p', text)));
 
 }

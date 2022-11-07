6bde0419b926f785d9489360a32019d1776eaef5
itsKD007 <78605466+itsKD007@users.noreply.github.com>
2022-11-07 16:17:49 +0530

    refactored server

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,8 @@
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
 import path from 'path';
 
-import { UserType, PORT, DB_PATH, LoginError } from './constants';
+import { PORT, DB_PATH, LoginRequest, LoginErrorMessage, LoginResponseBody, UserType} from './constants';
 import DatabaseDriver from './DatabaseDriver';
-
-interface LoginRequest extends Request {
-  body: {
-    username: string;
-    password: string;
-    type: UserType;
-  }
-}
 
 export default class App {
   private server = express();
@@ -26,39 +18,37 @@ export default class App {
     this.server.use(express.json());
 
     this.server.post('/api/login', async (req: LoginRequest, res: Response) => {
-      let errorMessage = null;
+      const responseBody: LoginResponseBody = {
+        success: false,
+        user: null,
+        error: null
+      };
       switch(req.body.type) {
         case 'customer':
           const customer = await this.database.getCustomer(req.body.username);
-          if(typeof customer == 'undefined') {
-            errorMessage = LoginError.NOT_FOUND;
+          if(customer == null) {
+            responseBody.error = LoginErrorMessage.NOT_FOUND;
             break;
           }
           if(customer.password != req.body.password) {
-            errorMessage = LoginError.WRONG_PASSWORD;
+            responseBody.error = LoginErrorMessage.WRONG_PASSWORD;
             break;
           }
-          res.send({
-            success: true,
-            user: {
-              username: customer.username,
-              name: customer.name,
-              email: customer.email,
-              phone: customer.phone,
-              type: 'customer'
-            },
-            error: null
-          });
+          responseBody.success = true;
+          responseBody.user = {
+            name: customer.name,
+            username: customer.username,
+            email: customer.email,
+            phone: customer.phone,
+            type: UserType.CUSTOMER
+          }
           return;
         default:
           break;
       }
 
-      res.send({
-        success: false,
-        user: null,
-        error: errorMessage
-      });
+      res.send(responseBody);
+
     });
 
   }

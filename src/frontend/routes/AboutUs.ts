862ea31362ba88b5570c0d88886b3b002af19032
itsKD007 <78605466+itsKD007@users.noreply.github.com>
2022-11-16 00:14:16 +0530

    implemented about us

--- src/frontend/routes/AboutUs.ts
+++ src/frontend/routes/AboutUs.ts
@@ -1,9 +1,21 @@
+import { el, mount } from 'redom';
+import { textToParagraphs } from '../utils';
+
 import Page from './abstract/Page';
 
 export default class AboutUs extends Page {
 
+  private text = `This is a prototype of an Insurance Management System created as a Software Engineering Project.
+
+ESC-501
+Kaustav Doari
+13000120049`;
+
+  private textElem = el('div.text', textToParagraphs(this.text));
+
   constructor() {
     super("About Us", "A Bit of Background");
+    mount(this.content, this.textElem);
   }
 
 }
